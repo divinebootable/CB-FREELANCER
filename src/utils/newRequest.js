453093ddb9c0a1bb0e5c40902fb0bc06386e3b80
dbootable@gmail.com <dbootable@gmail.com>
2024-09-22 15:09:03 +0100

    newRequest

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://cb-api-i6ne.onrender.com/",
   withCredentials: true,
 });
 
